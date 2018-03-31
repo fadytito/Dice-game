042541ad6a3838a8a4c56601f20733750e9a560e
fady tito <fadytito17@gmail.com>
2018-03-31 03:53:50 +0200

    add game rules

--- docs/app.js
+++ docs/app.js
@@ -107,6 +107,7 @@ var App = (function () {
 
         alert("GAME RULES:\n" +
 
+            "- Firstly set the FINAL score.\n" +
             "- The game has 2 players, playing in rounds.\n" +
             "- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score\n" +
             "- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn\n" +
